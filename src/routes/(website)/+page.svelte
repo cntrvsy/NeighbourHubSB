42936282529d976d3eff1010655057d6cb11676e
cntrvsy <benintangana@gmail.com>
2025-05-20 13:42:35 +0300

    pricing id was missing

--- src/routes/(website)/+page.svelte
+++ src/routes/(website)/+page.svelte
@@ -185,7 +185,7 @@
     </section>
     
     <!-- pricing -->
-    <section class="">
+    <section id="pricing">
       <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
           <Pricing />
@@ -234,4 +234,4 @@
         </div>
     </div>
     
-  </section>
+</section>
